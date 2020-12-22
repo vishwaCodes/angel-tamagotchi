79c11e12a3d76fd09235cf8512546b6b990b1d01
candytuft <vishnyc3@gmail.com>
2020-12-22 18:22:04 -0500

    Added event listeners to buttons

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 
 // Set functions for all the buttons 
 
-
+// Start Game 
 const tamaEgg = $('#egg-crack');
-
 
 function startButton () {
  tamaEgg.attr('src', "https://media.tenor.com/images/9cbc278974a310baf4d2ff9010c46d66/tenor.gif");
@@ -11,6 +10,41 @@ function startButton () {
 
 $('.startGame').on('click', startButton);
 
+// Feed me yummy food
+const tamaFeed = $('.feed');
+
+function feedTama () {
+    
+};
+
+$('.feed').on('click', tamaFeed);
 
 
+// Playtime
+const tamaPlay = $('.bored');
+
+function  playTama () {
+    
+};
+
+$('.bored').on('click', tamaPlay);
+
+
+// I need a nap
+const tamaNap = $('.lightsOff');
+
+function napTama () {
+    
+};
+
+$('.lightsOff').on('click', tamaNap);
+
+// Time to wake me up
+const tamaWake = $('.lightsOn');
+
+function wakeTama () {
+    
+};
+
+$('lightsOn').on('click', tamaWake);
 
