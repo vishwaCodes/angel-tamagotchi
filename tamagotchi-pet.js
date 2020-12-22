168dd552c2c04505547d3eb2074712a6be2856f5
candytuft <vishnyc3@gmail.com>
2020-12-21 20:46:15 -0500

    Initialized tamagotchi image

--- tamagotchi-pet.js
+++ tamagotchi-pet.js
@@ -1,1 +1,10 @@
 
+// Instatiate Tamagotchi with Image
+
+//const tamagotchiImage = document.getElementById('tamagotchi-image');
+//console.log(tamagotchiImage);
+
+
+const tamagotchiImage = $('tamagotchi-image');
+console.log(tamagotchiImage);
+
